5f3cd8de88d24f949013fa94de86757a542939cd
smjmoj <smjmoj@gmail.com>
2021-10-08 15:38:41 +0100

    Reason container would not start due to docker image version pulled in the EC2 Docker config pulling latest at a specific time. Now set to match that version.

--- jira_ecs_service/variables.tf
+++ jira_ecs_service/variables.tf
@@ -24,7 +24,7 @@ variable "jira_ecs_conf" {
 
   default = {
     image         = "docker.io/atlassian/jira-servicemanagement"
-    image_version = "4.16.1"
+    image_version = "4.17.0"
     service_port  = "8080"
     cpu           = "1024"
     memory        = "4096"
